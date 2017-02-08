da766dbac90c08fddc6fb85300856504b8d2297e
correadaniel3 <correadaniel3@gmail.com>
2017-02-08 13:47:13 -0500

    se agrego el socket pero hay un retraso

--- web/js/websocket.js
+++ web/js/websocket.js
@@ -1,15 +1,42 @@
 $(document).ready(function(){
     var rotation=0;
     var movimiento=0;
+    
+    var wsUri="ws://"+document.location.host+document.location.pathname+"rope"; 
+    var websocket = new WebSocket(wsUri);
+    
+    //se inicializan los metodos principales del web socket
+    websocket.onopen=function(evt){onOpen(evt);};
+    websocket.onmessage=function(evt){onMessage(evt);};
+    websocket.onerror=function(evt){onError(evt);};
+    
+    //Se envia el movimiento
+    function send_move(){
+        websocket.send(movimiento);   
+    }
+    
+    //funciones para el ingreso de usuarios
+    function onOpen(evt){
+        writeToScreen("Conectado a "+wsUri);
+    }
+    
+    //funcion para mover la bola al recibir la informacion
+    function onMessage(evt){
+        aux=evt.data;
+        mover(aux);
+    }
+    
     $("body").keyup(function(e) {
         if(e.keyCode === 39) {  //right
             movimiento=39;
+            send_move();
             mover(movimiento);
         }
     });
     $("body").keyup(function(e) {
         if(e.keyCode === 37) { //left
             movimiento=37;
+            send_move();
             mover(movimiento);
         }
     });
