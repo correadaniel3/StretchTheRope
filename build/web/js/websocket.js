d1d1a103c48454244ac21c59e865faea536dd53b
leoto17 <leoto17@hotmail.com>
2017-02-09 15:17:07 -0500

    Se agregaron cosillas por testear

--- build/web/js/websocket.js
+++ build/web/js/websocket.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
-    var rotation=0;
-    
-    $('span#pos').value=$("#ball").offset().left;
-    
+    var rotation=0;  
     var wsUri="ws://"+document.location.host+document.location.pathname+"rope"; 
     var websocket = new WebSocket(wsUri);
     
@@ -12,29 +9,44 @@ $(document).ready(function(){
     websocket.onerror=function(evt){onError(evt);};
     
     //Se envia el movimiento
-    function send_move(movimiento){
-        websocket.send(movimiento);   
+    function send_move(dato){
+        websocket.send(dato);   
     }
     
     //funciones para el ingreso de usuarios
     function onOpen(evt){
-        writeToScreen("Conectado a "+wsUri);
     }
     
     //funcion para mover la bola al recibir la informacion
     function onMessage(evt){
         aux=evt.data;
-        mover(aux);
+        console.log(aux);
+        if(aux!=116){
+            mover(aux);
+        }else if(aux==0){
+            alert("Ya hay 2 jugadores en partida, intenta más tarde!!");
+        }else if(aux==1){
+            alert("Jugador 1, mueve hacia la izquierda con la tecla A");
+        }else if(aux==2){
+            alert("Jugador 2, mueve hacia la derecha con la tecla D");
+        }else{
+            location.reload();
+        }
     }
     
     $("body").keyup(function(e) {
-        if(e.keyCode === 39) {  //right
+        if(e.keyCode === 68) {  //right
             send_move(39);
         }
     });
     $("body").keyup(function(e) {
-        if(e.keyCode === 37) { //left
+        if(e.keyCode === 65) { //left
             send_move(37);
+        }
+    });
+    $("body").keydown(function(e) {
+        if(e.keyCode === 116) { //reload
+            send_move(116);
         }
     });
     
@@ -49,6 +61,10 @@ $(document).ready(function(){
             $("#ball").rotate({angle:rotation,animateTo:rotation-30});
             rotation-=30;
         }
+    }
+    
+    function checkCollision(){
+        
     }
 });
 
--- src/java/com/udea/comunicaciones/ws/RopeServer.java
+++ src/java/com/udea/comunicaciones/ws/RopeServer.java
@@ -6,8 +6,10 @@
 package com.udea.comunicaciones.ws;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import javax.websocket.EncodeException;
 import javax.websocket.OnClose;
@@ -24,20 +26,36 @@ import javax.websocket.server.ServerEndpoint;
 public class RopeServer {
  //Creamos la lista de peers para usar el endpoint
     private static final Set<Session> peers = Collections.synchronizedSet(new HashSet<Session>());
+    private Session player1=null, player2=null;
+    private List ids = new ArrayList();
     @OnOpen
-    public void onOpen(Session peer){
+    public void onOpen(Session peer) throws IOException, EncodeException{
         //Adicionamos un nuevo peer a la colección
         peers.add(peer);
+        if(peers.toArray().length==1){
+            player1=peer;
+            onMessage("1",peer);
+        }else if(peers.toArray().length==2){
+            player2=peer;
+            onMessage("2",peer);
+        }else{
+            onMessage("0",peer);
+        }
     }
     @OnMessage
     public void onMessage(String message, Session client) throws IOException, EncodeException {
-        for(Session peer:peers){
-           peer.getBasicRemote().sendObject(message);
+        if(player1!=null && player2!=null){
+            for(Session peer:peers){
+               peer.getBasicRemote().sendObject(message);
+            }
+        }else{
+            client.getBasicRemote().sendObject("0");
         }
     }
     @OnClose
     public void onClose(Session peer){
         peers.remove(peer);
+        ids.remove(peer.getId());
     }
     
 }
